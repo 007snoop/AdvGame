d4c14488033a14a67ee406712483bd717f91b907
Colin Yetman <colinguitarist@gmail.com>
2025-09-03 12:31:13 -0230

    add super class for encounters - Built goblin entity to use monster super class - hopefully easier implamentation of encounters later

--- src/main/java/encounters/Goblin.java
+++ src/main/java/encounters/Goblin.java
@@ -1,83 +1,28 @@
 package encounters;
 
-public class Goblin {
+import entity.Monster;
 
-    // goblin stats
-    private String goblinArt;
-    private String name;
-    private int health;
-    private int attack;
-    private int strength;
-    private int defence;
-
-    // basic goblin encounter
+public class Goblin extends Monster {
+    // basic goblin encounter now uses super class
     public Goblin() {
-        this.name = "Goblin";
-        this.health = 15;
-        this.attack = 3;
-        this.strength = 3;
-        this.defence = 1;
-        this.goblinArt = """
-                         ,      ,
-                        /(.-""-.)\\
-                    |\\  \\/      \\/  /|
-                    | \\ / =.  .= \\ / |
-                    \\( \\   o\\/o   / )/
-                     \\_, '-/  \\-' ,_/
-                       /   \\__/   \\
-                       \\ \\__/\\__/ /
-                     ___\\ \\|--|/ /___
-                   /`    \\      /    `\\
-                  /       '----'       \\
-            """;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public int getDefence() {
-        return defence;
-    }
-
-    public String getGoblinArt() {
-        return goblinArt;
-    }
-
-    public void setGoblinArt(String goblinArt) {
-        this.goblinArt = goblinArt;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAttack(int attack) {
-        this.attack = attack;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public void setDefence(int defence) {
-        this.defence = defence;
+        super("Goblin"
+        ,15
+        ,3
+        ,3
+        ,1
+        , """
+                                                                               ,      ,
+                              /(.-""-.)\\
+                          |\\  \\/      \\/  /|
+                          | \\ / =.  .= \\ / |
+                          \\( \\   o\\/o   / )/
+                           \\_, '-/  \\-' ,_/
+                             /   \\__/   \\
+                             \\ \\__/\\__/ /
+                           ___\\ \\|--|/ /___
+                         /`    \\      /    `\\
+                        /       '----'       \\
+                        """);
     }
 }
 
--- src/main/java/entity/Monster.java
+++ src/main/java/entity/Monster.java
@@ -1,5 +1,82 @@
 package entity;
 //basic monster class to build other monsters from
 public abstract class Monster {
+    protected String name;
+    protected int health;
+    protected int attack;
+    protected int strength;
+    protected int defence;
+    protected String art;
 
+    // constructor for super
+    public Monster(String name, int health, int attack, int strength, int defence, String art) {
+        this.name = name;
+        this.art = art;
+        this.health = health;
+        this.attack = attack;
+        this.strength = strength;
+        this.defence = defence;
+    }
+
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public String getArt() {
+        return art;
+    }
+
+    //setters
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public void setArt(String art) {
+        this.art = art;
+    }
+
+    // shared behavior
+    public void takeDamage(int amount) {
+        health -= amount;
+        if (health < 0) health = 0;
+    }
+    public Boolean isAlive() {
+        return health > 0;
+    }
 }
