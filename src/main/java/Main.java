f15a8047a61bb81b2b5dca264d99d69c7799e1c9
Colin Yetman <colinguitarist@gmail.com>
2025-09-03 10:55:38 -0230

    Start Adventure project - Init commit - test/add goblin entity

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String goblinArt =
+                """
+                                   ,      ,
+                                  /(.-""-.)\\
+                              |\\  \\/      \\/  /|
+                              | \\ / =.  .= \\ / |
+                              \\( \\   o\\/o   / )/
+                               \\_, '-/  \\-' ,_/
+                                 /   \\__/   \\
+                                 \\ \\__/\\__/ /
+                               ___\\ \\|--|/ /___
+                             /`    \\      /    `\\
+                            /       '----'       \\""";
+
+        System.out.println(goblinArt);
+    }
+}
